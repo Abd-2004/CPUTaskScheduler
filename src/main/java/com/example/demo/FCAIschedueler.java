7677f0a66282439ee9b22989e7f76f15d274df4e
A-ZainEldin <ahmedzaineldin5@gmail.com>
2024-12-02 19:35:44 +0200

    I modified the inheritance architecture

--- src/main/java/com/example/demo/FCAIschedueler.java
+++ src/main/java/com/example/demo/FCAIschedueler.java
@@ -1,33 +1,42 @@
 package com.example.demo;
 
+import java.util.ArrayList;
+
 import static java.lang.Math.ceil;
 
-public class FCAIschedueler {
+
+import java.util.ArrayList;
+
+public class FCAIschedueler extends Scheduler {
 
     int calcFactor(int Priority, int Arrival, int V1, int RemBurst, int V2)
     {
         return (int)ceil((10 - Priority) + (Arrival / (float)V1) + (RemBurst / (float)V2));
     }
 
-    int getV1(Process[] processes, int n)
-    {
-        int last = Integer.MIN_VALUE;
-        for (int i = 0; i < n; i++)
+        int getV1(ArrayList<Process> processes)
         {
-            if (processes[i].getArrivalTime() > last)
-                last = processes[i].getArrivalTime();
+            int last = Integer.MIN_VALUE;
+            for (Process process : processes)
+            {
+                if (process.getArrivalTime() > last)
+                    last = process.getArrivalTime();
+            }
+            return last / 10;
         }
-        return last / 10;
-    }
+        int getV2(ArrayList<Process> processes)
+        {
+            int max = Integer.MIN_VALUE;
+            for (Process process : processes)
+            {
+                if (process.getBurstTime() > max)
+                    max = process.getBurstTime();
+            }
+            return max / 10;
+        }
 
-    int getV2(Process[] processes, int n)
-    {
-        int max = Integer.MIN_VALUE;
-        for (int i = 0; i < n; i++)
-        {
-            if (processes[i].getBurstTime() > max)
-                max = processes[i].getBurstTime();
-        }
-        return max / 10;
+    @Override
+    ArrayList<Integer> createSchedule(ArrayList<Process> processList) {
+        return null;
     }
 }

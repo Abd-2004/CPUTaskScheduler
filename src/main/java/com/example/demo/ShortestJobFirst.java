c40df4dd4f91b70d93e4e280bf8c5c8546980175
Kareemahmed2 <114743924+Kareemahmed2@users.noreply.github.com>
2024-12-09 00:05:01 +0200

    Aging threshold SJF update

--- src/main/java/com/example/demo/ShortestJobFirst.java
+++ src/main/java/com/example/demo/ShortestJobFirst.java
@@ -5,8 +5,8 @@ import java.util.*;
 public class ShortestJobFirst extends Scheduler{
     @Override
     public ArrayList<Integer> createSchedule(ArrayList<Process> processList) {
-        processList.sort(Comparator.comparing(Process::getArrivalTime).thenComparing(Process::getBurstTime));
-        Queue<Process> readyQueue = new PriorityQueue<>(Comparator.comparing(Process::getBurstTime).thenComparing(Process::getWaitingTime));
+        processList.sort(Comparator.comparing(Process::getArrivalTime));
+        Queue<Process> readyQueue = new PriorityQueue<>(Comparator.comparing(Process::getBurstTime));
         int totalTime= processList.stream().mapToInt(Process::getBurstTime).sum();  //loops to get the total running time for the processes
         ArrayList<Integer> schedule=new ArrayList<>(totalTime);  //the returned schedule for the graph representation
         int currentTime=0;  //used as a tracker for time and index for the schedule array
@@ -16,18 +16,25 @@ public class ShortestJobFirst extends Scheduler{
                 readyQueue.add(processList.get(arrivingProcess));
                 arrivingProcess++;
             }
-            for(Process process: readyQueue){  //responsible for aging updates for the processes in the ready queue
-                process.setWaitingTime(process.getWaitingTime()+1);
-            }
             if (readyQueue.isEmpty()) {  //handles if no process is currently using the CPU
                 schedule.add(currentTime, -1);
                 currentTime++;
                 continue;
             }
-            Process currentProcess = readyQueue.poll();  //pops the process with the highest priority
+            Process currentProcess = readyQueue.peek();  //pops the process with the highest priority
+            for (Process process: readyQueue) {  //responsible for aging updates for the processes in the ready queue
+                double threshold=1.5*currentProcess.getBurstTime();
+                if(process.getWaitingTime()>=threshold){
+                    currentProcess=process;
+                }
+            }
+            readyQueue.remove(currentProcess);
             for (int i = 0; i < currentProcess.getBurstTime(); i++) {  //handles updating the schedule array
                 schedule.add(currentTime, currentProcess.getPid());
                 currentTime++;
+            }
+            for(Process process: readyQueue){  //responsible for aging updates for the processes in the ready queue
+                process.setWaitingTime(process.getWaitingTime()+currentProcess.getBurstTime());
             }
         }
         return schedule;

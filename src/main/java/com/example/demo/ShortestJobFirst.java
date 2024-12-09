a46fca559f42529a144ced939dbc0e359da42316
Kareemahmed2 <114743924+Kareemahmed2@users.noreply.github.com>
2024-12-09 22:08:09 +0200

    Documentation commit

--- src/main/java/com/example/demo/ShortestJobFirst.java
+++ src/main/java/com/example/demo/ShortestJobFirst.java
@@ -14,23 +14,23 @@ public class ShortestJobFirst extends Scheduler{
         ArrayList<Integer> schedule = new ArrayList<>();
         int curTime = 0;
         while (!processList.isEmpty()) {
-            Process best = null;
-            for (Process p : processList) { //find which process should go next
+            Process best = null;  //a placeholder for the best process to be processed
+            for (Process p : processList) { //find the best process according to arrival time (criteria 1)
                 if (p.getArrivalTime() <= curTime) {
                     if (best == null) {
                         best = p;
                         continue;
                     }
-                    int bracket = p.getWaitingTime() / bracketLength;
+                    int bracket = p.getWaitingTime() / bracketLength;  //separates the present processes into intervals according to their waiting time
                     int bestBracket = best.getWaitingTime() / bracketLength;
-                    if (bracket != bestBracket) { //age brackets prevent starvation by prioritizing processes in higher brackets
+                    if (bracket != bestBracket) { //age brackets prevent starvation by prioritizing processes in higher brackets (aka the oldest process and criteria 2)
                         if (bracket > bestBracket) best = p;
                     }
-                    else if (p.getBurstTime() < best.getBurstTime()) best = p;
-                    else if (p.getBurstTime() == best.getBurstTime() && p.getArrivalTime() < best.getArrivalTime()) best = p;
+                    else if (p.getBurstTime() < best.getBurstTime()) best = p;  //compares according to burst time(crtieria 3)
+                    else if (p.getBurstTime() == best.getBurstTime() && p.getArrivalTime() < best.getArrivalTime()) best = p; //comapres according to arrival time as a last resort (criteria 4)
                 }
             }
-            if (best == null) { //no process needs to run at this time
+            if (best == null) { //handles the case the cpu being idle/no process is in the ready queue
                 schedule.add(-1);
                 curTime++;
                 continue;
@@ -39,10 +39,10 @@ public class ShortestJobFirst extends Scheduler{
                 for (Process p : processList) {
                     if (p != best && p.getArrivalTime() <= curTime) p.setWaitingTime(p.getWaitingTime()+1); //update waiting times for all processes that are not running now
                 }
-                schedule.add(best.getPid());
+                schedule.add(best.getPid());  //feeds the schedule with the currently being processed process
                 curTime++;
             }
-            processList.remove(best);
+            processList.remove(best);  //removes the process from the list
         }
         return schedule;
     }
